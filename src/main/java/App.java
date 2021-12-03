2ea8b9c8d4373017b9fc311600548fbaa1e3acdb
deepakbenny <deepakbenny@outlook.com>
2021-12-04 05:23:50 +0530

    solved day 1 problem set

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+/**
+ * @author dbenny
+ */
+public class App {
+
+	public static void main(String[] args) throws IOException {
+
+		SonarSweepDay1 sonarSweepDay1 = new SonarSweepDay1();
+		sonarSweepDay1.process();
+	}
+}
---
+++ src/main/java/SonarSweepDay1.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author dbenny
+ */
+public class SonarSweepDay1 {
+
+	private static final String INPUT_FILE = "src/main/resources/SonarSweepInput.txt";
+
+	public void process() throws IOException {
+
+		List<String> fileLines = Files.readAllLines(Paths.get(INPUT_FILE));
+
+		int previousIndex;
+		int currentIndex;
+
+		int previousEle;
+		int currentEle;
+
+		int counter = 0;
+		for (int i = 0; i < fileLines.size(); i ++) {
+
+			previousIndex = i - 1;
+			currentIndex = i;
+
+			if (previousIndex >= 0) {
+				previousEle = Integer.parseInt(fileLines.get(previousIndex));
+				currentEle = Integer.parseInt(fileLines.get(currentIndex));
+
+				if (currentEle > previousEle) {
+					counter ++;
+				}
+			}
+
+		}
+
+		System.out.println("number of increments="+ counter);
+	}
+}
